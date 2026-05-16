59e2ebbab9b42994b64380c6ff0c52d6e01749cc
Brent van Lieshout <70229620+Brentdevent@users.noreply.github.com>
2026-05-16 11:28:08 +0200

    Fix call stack spoofing

--- src/client/component/arxan/call_spoof.cpp
+++ src/client/component/arxan/call_spoof.cpp
@@ -6,26 +6,26 @@
 
 #include <stack>
 
+namespace arxan::detail
+{
+	void* callstack_proxy_addr{ nullptr };
+	thread_local const void* address_to_call{};
+
+	void set_address_to_call(const void* address)
+	{
+		address_to_call = address;
+	}
+}
+
 namespace arxan::call_spoof
 {
-	namespace detail
-	{
-		void* callstack_proxy_addr{ nullptr };
-		static thread_local const void* address_to_call{};
-
-		void set_address_to_call(const void* address)
-		{
-			address_to_call = address;
-		}
-	}
-
 	namespace
 	{
 		thread_local std::stack<uint64_t> address_stack{};
 
 		const void* get_address_to_call()
 		{
-			return detail::address_to_call;
+			return arxan::detail::address_to_call;
 		}
 
 		void store_address(const uint64_t address)
@@ -47,7 +47,7 @@ namespace arxan::call_spoof
 			a.pushad64();
 
 			a.call_aligned(get_stored_address);
-			a.mov(qword_ptr(rsp, 0x80), rax);
+			a.mov(qword_ptr(rsp, 0x78), rax);
 
 			a.popad64();
 
@@ -58,12 +58,18 @@ namespace arxan::call_spoof
 
 		uint64_t get_callstack_return_stub()
 		{
-			const auto placeholder = game::select(0x2CF1, 0x2CF1);
-			utils::hook::set<uint8_t>(placeholder - 2, 0xFF); // fakes a call
-			utils::hook::nop(placeholder, 1);
-			utils::hook::jump(placeholder + 1, utils::hook::assemble(callstack_return_stub));
+			static const auto stub = []()
+			{
+				const auto placeholder = game::select(0x9FFD79, 0x76F2F5);
 
-			return placeholder;
+				utils::hook::set<uint8_t>(placeholder - 2, 0xFF);
+				utils::hook::nop(placeholder, 1);
+				utils::hook::jump(placeholder + 1, utils::hook::assemble(callstack_return_stub));
+
+				return placeholder;
+			}();
+
+			return stub;
 		}
 
 		void callstack_stub(utils::hook::assembler& a)
@@ -72,13 +78,13 @@ namespace arxan::call_spoof
 
 			a.pushad64();
 			a.call_aligned(get_address_to_call);
-			a.mov(qword_ptr(rsp, 0x80), rax);
+			a.mov(qword_ptr(rsp, 0x78), rax);
 
-			a.mov(rcx, qword_ptr(rsp, 0x88));
+			a.mov(rcx, qword_ptr(rsp, 0x80));
 			a.call_aligned(store_address);
 
 			a.mov(rax, get_callstack_return_stub());
-			a.mov(qword_ptr(rsp, 0x88), rax);
+			a.mov(qword_ptr(rsp, 0x80), rax);
 
 			a.popad64();
 
@@ -87,4 +93,15 @@ namespace arxan::call_spoof
 			a.jmp(qword_ptr(rsp, -8));
 		}
 	}
+
+	struct component final : generic_component
+	{
+	public:
+		void post_unpack() override
+		{
+			arxan::detail::callstack_proxy_addr = utils::hook::assemble(callstack_stub);
+		}
+	};
 }
+
+REGISTER_COMPONENT(arxan::call_spoof::component)
