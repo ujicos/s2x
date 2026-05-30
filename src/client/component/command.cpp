cc6fee251c880d677f2d43ca437784f8a61915b5
Brent van Lieshout <70229620+Brentdevent@users.noreply.github.com>
2026-05-30 23:47:58 +0200

    Move dvar dump command

--- src/client/component/command.cpp
+++ src/client/component/command.cpp
@@ -304,42 +304,6 @@ namespace command
 			{
 				game::Com_Quit_f();
 			});
-
-			command::add("dvarDump", [](const params& argument)
-			{
-				console::info("================================ DVAR DUMP ========================================\n");
-				
-				std::string filename;
-				if (argument.size() == 2)
-				{
-					filename = "s2x/";
-					filename.append(argument[1]);
-
-					if (!filename.ends_with(".txt"))
-					{
-						filename.append(".txt");
-					}
-				}
-
-				for (auto i = 0; i < *game::dvarCount; i++)
-				{
-					auto* dvar = &game::dvarPool[i];
-
-					if (dvar)
-					{
-						if (!filename.empty())
-						{
-							const auto line = std::format("{} \"{}\"\r\n", dvar->name, game::Dvar_ValueToString(dvar, true, &dvar->current));
-							utils::io::write_file(filename, line, i != 0);
-						}
-
-						console::info("%s \"%s\"\n", dvar->name, game::Dvar_ValueToString(dvar, true, &dvar->current));
-					}
-				}
-
-				console::info("\n%i total dvars.\n", *game::dvarCount);
-				console::info("================================ END DVAR DUMP ====================================\n");
-			});
 		}
 	};
 }
--- src/client/component/dvars.cpp
+++ src/client/component/dvars.cpp
@@ -1,6 +1,8 @@
 #include <std_include.hpp>
 #include "loader/component_loader.hpp"
 
+#include "command.hpp"
+
 #include "game/game.hpp"
 
 #include "console/console.hpp"
@@ -160,6 +162,42 @@ namespace dvars
 		void post_unpack() override
 		{
 			dvar_find_malleable_var_hook.create(game::Dvar_FindMalleableVar, dvar_find_malleable_var_stub);
+
+			command::add("dvarDump", [](const command::params& argument)
+			{
+				console::info("================================ DVAR DUMP ========================================\n");
+
+				std::string filename;
+				if (argument.size() == 2)
+				{
+					filename = "s2x/";
+					filename.append(argument[1]);
+
+					if (!filename.ends_with(".txt"))
+					{
+						filename.append(".txt");
+					}
+				}
+
+				for (auto i = 0; i < *game::dvarCount; i++)
+				{
+					auto* dvar = &game::dvarPool[i];
+
+					if (dvar)
+					{
+						if (!filename.empty())
+						{
+							const auto line = std::format("{} \"{}\"\r\n", dvar->name, game::Dvar_ValueToString(dvar, true, &dvar->current));
+							utils::io::write_file(filename, line, i != 0);
+						}
+
+						console::info("%s \"%s\"\n", dvar->name, game::Dvar_ValueToString(dvar, true, &dvar->current));
+					}
+				}
+
+				console::info("\n%i total dvars.\n", *game::dvarCount);
+				console::info("================================ END DVAR DUMP ====================================\n");
+			});
 		}
 	};
 }
